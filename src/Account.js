9adf2ca9779292d341219b1c4198c0be4f3cd2d6
EuslidEuclid97 <zackandrews29@yahoo.com>
2022-05-01 17:38:04 -0500

    Cosmetic stuff

--- src/Account.js
+++ src/Account.js
@@ -1,16 +1,31 @@
 
-//import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import {AiOutlinePlus} from "react-icons/ai"
+import { Button } from "./Button";
 
 const Account = () => {
 
-    const {id} = useParams()
+    const {username} = useParams()
+
+    const addItem = () => {
+        console.log("Will add item")
+    }
 
     return ( 
         <div className="account">
-            <h1>Account - {id}</h1>
+            <h1>Account - {username}</h1>
+            <div className="buttonDiv">
+                <button className="button" onClick={addItem}>+</button>
+            </div>
         </div>
      );
 }
  
 export default Account;
+
+/* users need to be able to: 
+1. View their items
+2. Delete their items
+3. Add new items
+4. Log out
+*/
